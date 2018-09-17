b4e23b9d44bddbbcaae7c58c6eb7974b3ee3ebff
Lichunyin <licy2013@aliyun.com>
2018-09-17 15:05:36 +0800

    配合di容器自动注册功能做改动

---
+++ user/provider/UserProvider.php
@@ -0,0 +1,10 @@
+<?php
+namespace asbamboo\security\user\provider;
+
+/**
+ * 为了适应asbamboo/di中的自动注册为服务的功能而添加
+ *
+ * @author 李春寅 <licy2013@aliyun.com>
+ * @since 2018年9月17日
+ */
+class UserProvider extends MemoryUserProvider{}
